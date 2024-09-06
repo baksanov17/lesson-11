a8fe82ea2827963c126e903bcb420f0c7872cc5b
baksanov17 <baksanov17@mail.ru>
2024-09-06 19:53:10 +0500

    Add files via upload

--- For.py
+++ For.py
@@ -1,13 +1,13 @@
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+numbers = [9, 2, 3, 4, 11, 23, 1, 10, 15, 12, 99]
 primes = []
 not_primes = []
 for i in numbers:
     simple = True
-    for g in numbers[1:]:
-        if i > 1 and i % g == 0 and i > g:
+    for g in numbers:
+        if i > 1 and g > 1 and i % g == 0 and i > g:
             simple = False
             break
-    if simple == True and i > 1:
+    if i > 1 and g > 1 and simple == True:
         primes += [i]
     elif simple == False:
         not_primes += [i]
